b27fff2f0e2b52846cf2b7d2915f566f3178c278
Brandon <235469637+brandon-04@users.noreply.github.com>
2026-05-19 16:03:21 +0100

    added live timer project

---
+++ project-examples/live-time-project/script.js
@@ -0,0 +1,30 @@
+let display = document.querySelector("#display");
+
+setInterval(() => {
+    updateDisplay();
+},50);
+
+function updateDisplay() {
+    let fullDate = new Date();
+    let months = ["Jan", "Feb",  "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", ]
+    //year = month = day = | time =  : :
+
+    let year = fullDate.getFullYear();
+    let month = months[fullDate.getMonth()];
+    let day = days[fullDate.getDay()];
+
+    let hour = fullDate.getHours();
+    let minute = addZero(fullDate.getMinutes());
+    let second = addZero(fullDate.getSeconds());
+
+    let newVal = `year = ${year} | month = ${month} | day = ${day} | time =  ${hour} : ${minute} : ${second}`;
+
+    display.textContent = newVal
+
+    console.log(hour);
+}
+
+function addZero(val) {
+    return val < 10 ? "0" + val : val;
+}
