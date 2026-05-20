91254dc464dc95be2ec42b44bab8c3dc95d56bda
brandon <235469637+brandon-04@users.noreply.github.com>
2026-05-20 12:00:38 +0100

    added etching functionality

--- project-examples/etch-a-sketch-project/script.js
+++ project-examples/etch-a-sketch-project/script.js
@@ -1,16 +1,56 @@
 let container = document.querySelector("#container");
+let resetButton = document.querySelector("#reset");
+
+let cellObjects = [];
 
 let gridDimension = 50;
-let cellPercentage = 100/gridDimension;
+let cellPercentage = `${100/gridDimension}%`;
 
-for(let i = 0; i < gridDimension; i++ ) {
-    let row = document.createElement("div");
-    row.setAttribute("class","row");
-    // row.setAttribute("height",`${cellPercentage}%`)
+resetButton.addEventListener("click", () => {
+    clearGrid();
+});
 
-    for(let x = 0; x < gridDimension; x++) {
-        let cell = document.createElement("div");
+gridGeneration();
 
+function gridGeneration() {
+    for(let i = 0; i < gridDimension; i++ ) {
+        let row = document.createElement("div");
+        row.setAttribute("class","row");
+        row.style.height = cellPercentage;
+    
+        for(let x = 0; x < gridDimension; x++) {
+            let cell = document.createElement("div");
+            let cellID = `cell${i}-${x}`;
+            cell.setAttribute("class", "cell");
+            cell.setAttribute("id", cellID)
+            cell.style.width = cellPercentage;
+
+
+            cell.addEventListener("mouseover", () => {
+                cell.style.backgroundColor = ;
+            });
+    
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+}
+
+function clearGrid() {
+    for(let i = 0; i < gridDimension; i++ ) {
+        for(let x = 0; x < gridDimension; x++) {
+            let cellID = `cell${i}-${x}`;
+            let cell = document.querySelector(`#${cellID}`);
+
+            cell.style.backgroundColor = "white";
+        }
+    }
+}
+
+function getRandomColor() {
+    
+}
+
+function getRandomNum() {
+    
 }
