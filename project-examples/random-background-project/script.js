dd550cbe39ed464297356543642f211ce53c0dc2
brandon <235469637+brandon-04@users.noreply.github.com>
2026-05-20 16:44:51 +0100

    added to background project

--- project-examples/random-background-project/script.js
+++ project-examples/random-background-project/script.js
@@ -3,18 +3,20 @@ let colorDisplay = document.querySelector("#color-display");
 let changeButton = document.querySelector("#change-button");
 
 changeButton.addEventListener("click", () => {
-    updateColor();
+    setTimeout(() => {
+        updateColor();
+    },100);
 });
 
 function updateColor() {
     let hex = getRandomHexCode();
 
     colorDisplay.textContent = `color: ${hex}`;
-    page.style.backgroundColor = hex;
+    page.style.backgroundColor = hex; 
 }
 
 function getRandomHexCode() {
-    let arr = ["A","B","C","D","E","F","G","0","1","2","3","4","5","6","7","8","9"];
+    let arr = ["a","b","c","d","e","f","g","0","1","2","3","4","5","6","7","8","9"];
     let hex = "";
 
     for(let i = 0; i < 6; i++) {
