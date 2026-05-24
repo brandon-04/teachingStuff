cce9a1f836ed011002a0a3663146928431d0d3da
brandon <235469637+brandon-04@users.noreply.github.com>
2026-05-24 18:18:54 +0100

    added day of week problem

--- project-examples/base-js-projects/script.js
+++ project-examples/base-js-projects/script.js
@@ -2,11 +2,10 @@ let fizzBuzzButton = document.querySelector("#fizz-buzz").addEventListener("clic
 let fooFizzBuzzBarButton = document.querySelector("#foo-fizz-buzz-bar").addEventListener("click", fooFizzBuzzBar)
 let ninetyNineBottles = document.querySelector("#ninety-nine-bottles").addEventListener("click", ninetyNineBottlesOfBeer)
 
-
-function fizzBuzz() {
 // Print the numbers from 1 to 100 inclusive, each on their own line.
 // If, however, the number is a multiple of three then print Fizz instead, and if the number is a multiple of five then print Buzz.
 // If multiple conditions hold true then all replacements should be printed, for example 15 should print FizzBuzz. 
+function fizzBuzz() {
     for(let i = 1; i <= 100; i++) {
         let multipleOfThree = i % 3 == 0 ? true : false;
         let multipleOfFive = i % 5 == 0 ? true : false;
@@ -26,13 +25,13 @@ function fizzBuzz() {
     }
 }
 
-function fooFizzBuzzBar() {
 // Print the numbers from 1 to 1,000 inclusive, each on their own line.
 // If, however, the number is a multiple of two then print Foo instead, 
 // if the number is a multiple of three then print Fizz, 
 // if the number is a multiple of five then print Buzz, 
 // and if the number is a multiple of seven then print Bar.
 // If multiple conditions hold true then all replacements should be printed, for example 15 should print FizzBuzz. 
+function fooFizzBuzzBar() {
 
     for(let i = 1; i < 1000; i++) {
         let multipleOfTwo = i % 2 == 0 ? true : false;
@@ -50,7 +49,6 @@ function fooFizzBuzzBar() {
      }
 }
 
-function ninetyNineBottlesOfBeer() {
 // Print the lyrics to the song 99 Bottles of Beer:
 
 // 99 bottles of beer on the wall, 99 bottles of beer.
@@ -64,9 +62,14 @@ function ninetyNineBottlesOfBeer() {
 
 // No more bottles of beer on the wall, no more bottles of beer.
 // Go to the store and buy some more, 99 bottles of beer on the wall.
-
+function ninetyNineBottlesOfBeer() {
     for(let i = 99; i >= 0; i--) {
         let val = i > 0 ? i : "No more";
         console.log(`${val} bottles of beer on the wall, ${val} bottles of beer.\n` + `Take one down and pass it around, ${isNaN(val) ? val : val - 1} bottles of beer on the wall.`);
     } 
+}
+
+// Given a date in the YYYY-MM-DD format between 1583-01-01 and 9999-12-31 inclusive, output the English name of the corresponding day of the week. 
+function dayOfWeek() {
+    
 }
