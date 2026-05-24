cac660238a708cdab9a6dae6c4c665a6e71e9054
brandon <235469637+brandon-04@users.noreply.github.com>
2026-05-24 17:34:31 +0100

    added 99 bottles problem

--- project-examples/base-js-projects/script.js
+++ project-examples/base-js-projects/script.js
@@ -1,6 +1,6 @@
 let fizzBuzzButton = document.querySelector("#fizz-buzz").addEventListener("click", fizzBuzz);
 let fooFizzBuzzBarButton = document.querySelector("#foo-fizz-buzz-bar").addEventListener("click", fooFizzBuzzBar)
-
+let ninetyNineBottles = document.querySelector("#ninety-nine-bottles").addEventListener("click", ninetyNineBottlesOfBeer)
 
 
 function fizzBuzz() {
@@ -48,4 +48,25 @@ function fooFizzBuzzBar() {
                 ${multipleOfFive ? "Buzz" : ""}${mutltipleOfSeven ? "Bar" : ""}`)
         }
      }
+}
+
+function ninetyNineBottlesOfBeer() {
+// Print the lyrics to the song 99 Bottles of Beer:
+
+// 99 bottles of beer on the wall, 99 bottles of beer.
+// Take one down and pass it around, 98 bottles of beer on the wall.
+
+// 98 bottles of beer on the wall, 98 bottles of beer.
+// Take one down and pass it around, 97 bottles of beer on the wall.
+// …
+// 1 bottle of beer on the wall, 1 bottle of beer.
+// Take one down and pass it around, no more bottles of beer on the wall.
+
+// No more bottles of beer on the wall, no more bottles of beer.
+// Go to the store and buy some more, 99 bottles of beer on the wall.
+
+    for(let i = 99; i >= 0; i--) {
+        let val = i > 0 ? i : "No more";
+        console.log(`${val} bottles of beer on the wall, ${val} bottles of beer.\n` + `Take one down and pass it around, ${isNaN(val) ? val : val - 1} bottles of beer on the wall.`);
+    } 
 }
